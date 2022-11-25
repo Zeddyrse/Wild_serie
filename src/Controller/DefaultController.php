f6d3ad51e45d1c6080f70943a633147e50124a72
jeremy <jeremy.guimard.pro@gmail.com>
2022-11-25 11:34:27 +0100

    soluce quest 5

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,10 +6,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-     
-#[Route('/', name: 'app_index')]
+    
 class DefaultController extends AbstractController
 {
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
         return $this->render('index.html.twig');
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,21 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-    #[Route('/program/', name: 'program_index')]
+
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
          ]);
+    }
+
+    #[Route('/{id}/', name: 'show', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function show(int $id = 1): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => 4]);
     }
 }
